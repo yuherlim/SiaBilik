5cbcb6d01fc1b9daa6db8795a0194bda951594ee
Johnson Ong <ong5828@gmail.com>
2024-05-21 04:25:36 +0800

    UserManagment UI update

--- app/src/main/java/UserManagement/EditPassword.kt
+++ app/src/main/java/UserManagement/EditPassword.kt
@@ -1,10 +1,11 @@
-package com.example.siabilik
+package UserManagement
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.siabilik.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [editProfile.newInstance] factory method to
+ * Use the [EditPassword.newInstance] factory method to
  * create an instance of this fragment.
  */
-class editProfile : Fragment() {
+class EditPassword : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class editProfile : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_profile, container, false)
+        return inflater.inflate(R.layout.fragment_edit_password, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class editProfile : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment editProfile.
+         * @return A new instance of fragment EditPassword.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            editProfile().apply {
+            EditPassword().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
