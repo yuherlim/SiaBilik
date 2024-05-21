a0a338fb78ee3df64daf3bc97e41ae325c2d1c6e
yuherlim <110385648+yuherlim@users.noreply.github.com>
2024-05-21 16:05:13 +0800

    revert gradle changes to before firebase.auth

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,9 +53,7 @@ dependencies {
     implementation("com.google.android.material:material:1.13.0-alpha02")
 
     // for firebase
-    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation("com.google.firebase:firebase-firestore:24.11.0")
-    implementation(libs.firebase.auth.ktx)
 
     // for navigation
     implementation(libs.androidx.navigation.fragment.ktx)
