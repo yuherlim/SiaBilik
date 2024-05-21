4465f4a50d5566a816d0fb3e1bbb7d51f3626c3f
yuherlim <110385648+yuherlim@users.noreply.github.com>
2024-05-21 22:15:53 +0800

    progress

--- app/src/main/java/com/example/siabilik/MainActivity.kt
+++ app/src/main/java/com/example/siabilik/MainActivity.kt
@@ -6,62 +6,34 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.siabilik.UserManagement.RegisterFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.siabilik.databinding.ActivityMainBinding
-import com.example.siabilik.ownerAcc.ui.Listing
-import com.example.siabilik.ownerAcc.ui.MyListing
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val nav by lazy { supportFragmentManager.findFragmentById(R.id.host)!!.findNavController() }
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private val topLevelDestinations = setOf(
-        R.id.tenantViewListingsFragment, R.id.tenantViewStarredListingsFragment, R.id.tenantViewRequestsFragment, R.id.tenantAccountFragment,
-        )
+    private lateinit var abc: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//        super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
-//        setContentView(R.layout.activity_main)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//
-//        }
-//
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//        replaceFragment(MyListing())
 
-        // Action bar
+
+        abc = AppBarConfiguration(
+            setOf(
+                R.id.tenantViewListingsFragment,
+                R.id.tenantViewStarredListingsFragment,
+                R.id.tenantViewRequestsFragment,
+                R.id.tenantAccountFragment,
+            )
+        )
+
+        // Action bar and bottom nav
         setSupportActionBar(binding.topAppBar)
-        // appBarConfiguration needed to initialize app bar so that up button won't be displayed at top level destinations
-        appBarConfiguration = AppBarConfiguration(topLevelDestinations)
-        setupActionBarWithNavController(nav, appBarConfiguration)
-
-
-
-
-
-        binding.bv.setOnItemSelectedListener {
-
-            when(it.itemId){
-
-                R.id.listing -> replaceFragment(Listing())
-                R.id.myListing -> replaceFragment(MyListing())
-                R.id.profile -> replaceFragment(RegisterFragment())
-                R.id.profile -> replaceFragment(RegisterFragment())
-                else ->{
-
-                }
-
-            }
-            true
-        }
+        setupActionBarWithNavController(nav, abc)
+        binding.bv.setupWithNavController(nav)
 
     }
 
