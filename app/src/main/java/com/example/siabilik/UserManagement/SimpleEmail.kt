286c287e3ef2d0cab60f40eeef98d587912a86a9
yuherlim <110385648+yuherlim@users.noreply.github.com>
2024-05-22 11:48:19 +0800

    commit

--- app/src/main/java/com/example/siabilik/UserManagement/SimpleEmail.kt
+++ app/src/main/java/com/example/siabilik/UserManagement/SimpleEmail.kt
@@ -7,7 +7,6 @@ import java.net.PasswordAuthentication
 import java.util.Properties
 import javax.mail.Authenticator
 import javax.mail.Message
-import javax.mail.PasswordAuthentication
 import javax.mail.Session
 import javax.mail.Transport
 import javax.mail.internet.MimeMessage
